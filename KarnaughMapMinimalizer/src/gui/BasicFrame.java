2164887c9297aa3d932f399828116d6653c85f8e
syky27 <syky27@gmail.com>
2013-03-22 01:34:36 +0100

    BreakThrought!!

--- KarnaughMapMinimalizer/src/gui/BasicFrame.java
+++ KarnaughMapMinimalizer/src/gui/BasicFrame.java
@@ -20,23 +20,23 @@ import javax.swing.SwingUtilities;
  * @author syky
  */
 public class BasicFrame {
+
+    protected JFrame frame = new JFrame("Minimalizer");
+    protected JMenuBar menubar = new JMenuBar();
+    protected JMenu file = new JMenu("File");
+    protected JMenu edit = new JMenu("Edit");
+    protected JMenu about = new JMenu("About");
+    protected JMenu fnc = new JMenu("Logic Functions...");
     
-        protected JFrame frame = new JFrame("Minimalizer");
-        protected JMenuBar menubar = new JMenuBar();
-        protected JMenu file = new JMenu("File");
-        protected JMenu edit = new JMenu("Edit");
-        protected JMenu about = new JMenu("About");
-        protected JMenu fnc = new JMenu("Logic Functions...");
-        protected JMenuItem exit = new JMenuItem("Exit");
-        protected JMenuItem author = new JMenuItem("Author");
-        protected JMenuItem fnc3 = new JMenuItem("3 Functions");
-        protected JMenuItem fnc4 = new JMenuItem("4 Functions");
-        protected JMenuItem fnc5 = new JMenuItem("5 Functions");
-        protected JMenuItem fnc6 = new JMenuItem("May be continued...");
-        
-        protected MapButton x = new MapButton();
-        
-        protected final JButton button = new JButton("1");
+    protected JMenuItem exit = new JMenuItem("Exit");
+    protected JMenuItem author = new JMenuItem("Author");
+    protected JMenuItem license = new JMenuItem("License");
+    protected JMenuItem fnc3 = new JMenuItem("3 Functions");
+    protected JMenuItem fnc4 = new JMenuItem("4 Functions");
+    protected JMenuItem fnc5 = new JMenuItem("5 Functions");
+    protected JMenuItem fnc6 = new JMenuItem("May be continued...");
+    protected MapButton x = new MapButton();
+    protected final JButton button = new JButton("1");
 
     public BasicFrame() {
         frame.setVisible(true);
@@ -44,100 +44,48 @@ public class BasicFrame {
         frame.setSize(640, 480);
         frame.setResizable(false);
         frame.setJMenuBar(menubar);
+        
+        
         menubar.add(file);
         menubar.add(edit);
         menubar.add(about);
-        
-        file.add(exit);
+
+      
         file.add(fnc);
+        file.add(exit);
+        
         fnc.add(fnc3);
         fnc.add(fnc4);
         fnc.add(fnc5);
         fnc.add(fnc6);
-        
-        about.add(author); 
-        frame.add(x.button);
 
-        
-    exit.addActionListener(new ExitAction());
-    author.addActionListener(new AuthorAction());
-    
+        about.add(author);
+        about.add(license);
+        //frame.add(x.button);
+
+        //listerners for menu button actions
+        exit.addActionListener(new ExitAction());
+        author.addActionListener(new AuthorAction());
+
     }
-    
-    public void assignButton(JButton button){
+
+    protected void assignButton(JButton button) {
         this.frame.add(button);
     }
-    
-    
+
     static class ExitAction implements ActionListener {
+
         @Override
-        public void actionPerformed(ActionEvent e){
+        public void actionPerformed(ActionEvent e) {
             System.exit(0);
         }
     }
-    
-    static class AuthorAction implements ActionListener{
+
+    static class AuthorAction implements ActionListener {
+
         @Override
-        public void actionPerformed(ActionEvent e){
+        public void actionPerformed(ActionEvent e) {
             System.exit(1);
         }
     }
-
-        
-    
-
-    public void  getTestingButton() {
-        button.addMouseListener(new MouseAdapter(){
-            boolean pressed;
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-                button.getModel().setArmed(true);
-                button.getModel().setPressed(true);
-                pressed = true;
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                //if(isRightButtonPressed) {underlyingButton.getModel().setPressed(true));
-                button.getModel().setArmed(false);
-                button.getModel().setPressed(false);
-
-                if (pressed) {
-                    if (SwingUtilities.isLeftMouseButton(e) && !("1".equals(button.getText()))) {
-                        button.setText("1");
-                    }else if(SwingUtilities.isLeftMouseButton(e) && ("1".equals(button.getText()))){
-                        button.setText("0");
-                    } 
-                    else {
-                        button.setText("X");
-                    }
-                }
-                pressed = false;
-
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-                pressed = false;
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                pressed = true;
-            }                    
-        
-        });
-
-        
-        
-  
-        
-        
-        
-        
-        
-    }
 }
-
-
--- KarnaughMapMinimalizer/src/gui/MainFrame.java
+++ KarnaughMapMinimalizer/src/gui/MainFrame.java
@@ -1,26 +1,29 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package gui;
 
 import javax.swing.JButton;
 
-/**
- *
- * @author syky
- */
 public class MainFrame extends BasicFrame{
 
     MapButton but_0 =  new MapButton();
+
+    public MainFrame() {
+        assignButton(but_0.button);
+    }
     
+    
+    
+
    
 
 
     
     
-    
+    protected void assignButton(JButton button) {
+        super.assignButton(button);
+    }
     
     
     
 }
+   
+   
