9ed2be229c205968c76eda81c297469af5f8e0ae
syky27 <syky27@gmail.com>
2013-03-22 03:43:15 +0100

    sync

---
+++ KarnaughMapMinimalizer/src/logic/ThreeFunction.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+/**
+ *
+ * @author syky
+ */
+public class ThreeFunction extends Evaluation{
+
+    public ThreeFunction() {
+    }
+    
+    
+}
