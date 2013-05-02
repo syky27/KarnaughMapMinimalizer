91f75efc9abe9ea6b93e57f63f611b370424e791
syky27 <syky27@gmail.com>
2013-05-02 18:49:47 +0200

    Fixing some minor bugs....

--- KarnaughMapMinimalizer/src/karnaughmapminimalizer/KarnaughMapMinimalizer.java
+++ KarnaughMapMinimalizer/src/karnaughmapminimalizer/KarnaughMapMinimalizer.java
@@ -1,34 +1,13 @@
 package karnaughmapminimalizer;
 
-import com.sun.corba.se.impl.protocol.JIDLLocalCRDImpl;
-import gui.BasicFrame;
-import gui.FourFunctionFrame;
 import gui.MyFrame;
-import javax.swing.JInternalFrame;
 
 public class KarnaughMapMinimalizer {
 
     public static void main(String[] args) {
-        //BasicFrame frame = new BasicFrame();
-        
         MyFrame fr = new MyFrame();
         fr.setVisible(true);
     }
-    
-//    
-//    void  add(JInternalFrame f) {
-//        
-//        this.fr.addJIn(f);
-//
-//    }
-     
-     
-     
-    
-    
-    
-    
-    
 }
     
     
