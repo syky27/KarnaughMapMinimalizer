bfdfd3988f0e1e3689c752251de55bced23c1e12
syky27 <syky27@gmail.com>
2013-05-05 13:54:47 +0200

    Threading works lags are fixed....

--- KarnaughMapMinimalizer/src/FourLogic/FourEvalOperations.java
+++ KarnaughMapMinimalizer/src/FourLogic/FourEvalOperations.java
@@ -3,15 +3,6 @@ package FourLogic;
 
 import java.util.ArrayList;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author dave
- */
 public class FourEvalOperations {
     
     public void additem(FourEval list,int product[]){
